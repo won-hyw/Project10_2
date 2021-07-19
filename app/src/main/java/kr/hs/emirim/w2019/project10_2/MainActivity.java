bb1c3153501c2e0391fae1bff9b729c31e3e99d1
choi ji won <w2019@e-mirim.hs.kr>
2021-07-19 15:16:36 +0900

    Completion MainActivity

--- app/src/main/java/kr/hs/emirim/w2019/project10_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project10_2/MainActivity.java
@@ -3,12 +3,33 @@ package kr.hs.emirim.w2019.project10_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String[] imgNames = {"Your Choice DINO", "INCOMPLETE DINO", "DICON DINO", "DICON DINO", "INCOMPLETE DINO", "ESQUIRE DINO", "Flower DINO", "Lovely DINO", "Cute DINO"};
+    int[] imgVIds = {R.id.img01, R.id.img02, R.id.img03, R.id.img04, R.id.img05, R.id.img06, R.id.img07, R.id.img08, R.id.img09};
+    int[] voteCount = new int[imgVIds.length];
+    ImageView[] image = new ImageView[imgVIds.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.title);
         setContentView(R.layout.activity_main);
+
+        for(int i = 0; i < imgVIds.length; i++) {
+            final int index;
+            index = i;
+            image[index] = findViewById(imgVIds[index]);
+            image[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(), imgNames[index] + " 총 " + voteCount[index] + "표", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
---
+++ app/src/main/java/kr/hs/emirim/w2019/project10_2/ResultActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.w2019.project10_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
