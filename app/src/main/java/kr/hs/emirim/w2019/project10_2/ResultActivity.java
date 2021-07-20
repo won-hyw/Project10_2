6b62ae7a2e9fbc1149ce46cfc8679bcc601b1747
choi ji won <w2019@e-mirim.hs.kr>
2021-07-20 11:08:49 +0900

    Completion DirSolution1-2 ResultActivity Commit

--- app/src/main/java/kr/hs/emirim/w2019/project10_2/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project10_2/ResultActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -25,6 +26,7 @@ public class ResultActivity extends AppCompatActivity {
 
         int[] tvIds = {R.id.text1, R.id.text2, R.id.text3, R.id.text4, R.id.text5, R.id.text6, R.id.text7, R.id.text8, R.id.text9};
         int[] rbIds = {R.id.rating1, R.id.rating2, R.id.rating3, R.id.rating4, R.id.rating5, R.id.rating6, R.id.rating7, R.id.rating8, R.id.rating9};
+        int[] imgSrcIds = {R.drawable.i01, R.drawable.i02, R.drawable.i03, R.drawable.i04, R.drawable.i05, R.drawable.i06, R.drawable.i07, R.drawable.i08, R.drawable.i09};
 
         for(int i = 0; i < tv.length; i++) {
             tv[i] = findViewById(tvIds[i]);
@@ -32,6 +34,21 @@ public class ResultActivity extends AppCompatActivity {
             tv[i].setText(imgNames[i]);
             rb[i].setRating(voteCount[i]);
         }
+
+        int max = 0;
+        int index = 0;
+        for(int i = 0; i < voteCount.length; i++ ) {
+            if(max < voteCount[i]){
+                max = voteCount[i];
+                index = i;
+            }
+        }
+
+        TextView textTitle = findViewById(R.id.text_title);
+        ImageView imgV = findViewById(R.id.imgv);
+
+        textTitle.setText(imgNames[index]);
+        imgV.setImageResource(imgSrcIds[index]);
 
         Button btnBack = findViewById(R.id.btn_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
